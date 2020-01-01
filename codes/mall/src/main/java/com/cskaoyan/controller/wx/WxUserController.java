5e377b64e4dcb146e785cbafbb17b0c1a2a71cc8
Pined <x527602546@163.com>
2020-01-01 13:49:56 +0800

    commit by Pined date:202001011349

--- codes/mall/src/main/java/com/cskaoyan/controller/wx/WxUserController.java
+++ codes/mall/src/main/java/com/cskaoyan/controller/wx/WxUserController.java
@@ -6,6 +6,7 @@ import com.cskaoyan.bean.LoginBean;
 import com.cskaoyan.bean.User;
 import com.cskaoyan.shiro.MallToken;
 import com.cskaoyan.util.Md5Util;
+import com.fasterxml.jackson.databind.ser.Serializers;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.subject.Subject;
@@ -19,6 +20,10 @@ import java.util.Map;
 
 @RestController
 public class WxUserController {
+
+
+
+    /*--------------------------------------User/auth-----------------------------------------------*/
 
     /**
      * 微信端登录
@@ -76,5 +81,59 @@ public class WxUserController {
         baseRespVo.setErrno(0);
         baseRespVo.setErrmsg("成功");
         return baseRespVo;
+    }
+
+    /**
+     * 假回复
+     * 用户登录后会显示其订单信息
+     * @return
+     */
+    @RequestMapping("wx/user/index")
+    public String userIndex(){
+        //拿到参数后也可以修改String语句，拼接的方法返回json字符串，最初的方法
+        String resp = "{\"errno\":0,\"data\":{\"order\":{\"unrecv\":0,\"uncomment\":0,\"unpaid\":1,\"unship\":0}},\"errmsg\":\"成功\"}";
+        return resp;
+    }
+
+
+    /**
+     * 发送验证码的
+     */
+    @RequestMapping("wx/auth/regCaptcha")
+    public String authReg(){
+        String resp = "{\"errno\":701,\"errmsg\":\"小程序后台验证码服务不支持。 Ps : 别请求了，直接用lanzhao,密码123登录吧\"}";
+        return resp;
+    }
+
+
+    /**
+     * 有空了做一下验证码
+     * @return
+     */
+    @RequestMapping("wx/auth/register")
+    public String register(){
+        String resp = "{\"errno\":703,\"errmsg\":\"验证码错误\"}";
+        return resp;
+    }
+
+    /**
+     * 微信登录
+     * 真的能拿到你的微信信息
+     */
+    @RequestMapping("wx/auth/login_by_weixin")
+    public String login_by_weixin(){
+        String resp = "{\"errno\":-1,\"errmsg\":\"错误\"}";
+        return resp;
+    }
+
+    /**
+     * 密码重置
+     * 等验证码弄好了试试
+     * @return
+     */
+    @RequestMapping("wx/auth/reset")
+    public String reset(){
+        String resp = "{\"errno\":703,\"errmsg\":\"验证码错误\"}";
+        return resp;
     }
 }
